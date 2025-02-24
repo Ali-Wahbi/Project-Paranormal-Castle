41e14bd7439e19a633e82d7b75e1e14816fe93b6
Ali-Wahbi <aliwahbi436@gmail.com>
2025-02-24 14:02:20 +0400

    General
    Main area add modules
    locked doors pop up

--- Assets/Scripts/MainArea/DoorsManager.cs
+++ Assets/Scripts/MainArea/DoorsManager.cs
@@ -6,19 +6,20 @@ using System.Linq;
 
 public class DoorsManager : MonoBehaviour
 {
-    
+
     // door handler should handle when a door is clicked, it saves the position infront of it
     // so that when the player returns through that door, he is spawned in that position
     // 
     [SerializeField] GeneralGameManager PlayerManager;
     public Transform lastUsedDoor;
 
-    
+
     string fullPath = Application.dataPath + "/Saves/";
     public string SaveFile = "PosSave";
     string fileName;
 
-    private void Start() {
+    private void Start()
+    {
         fileName = SaveFile + ".txt";
         Load();
         PlayerManager.SetStartPosition(lastUsedDoor);
@@ -26,20 +27,23 @@ public class DoorsManager : MonoBehaviour
 
 
     // called from doors used to go from area to another
-    public void SetLastUsed(Transform lastDoor){
+    public void SetLastUsed(Transform lastDoor)
+    {
         lastUsedDoor = lastDoor;
 
         Save();
     }
 
-    void Save(){
-        PosSaver saver = new PosSaver{
+    void Save()
+    {
+        PosSaver saver = new PosSaver
+        {
             pos = lastUsedDoor.position,
             rot = lastUsedDoor.parent.localEulerAngles
         };
 
         string saveString = JsonUtility.ToJson(saver);
-        
+
         CheckFileDir();
 
         File.WriteAllText(fullPath + fileName, saveString);
@@ -47,29 +51,38 @@ public class DoorsManager : MonoBehaviour
 
     }
 
-    void Load(){
-        if (File.Exists(fullPath + fileName)){
+    void Load()
+    {
+        if (File.Exists(fullPath + fileName))
+        {
             string loadString = File.ReadAllText(fullPath + fileName);
             // Debug.Log("Loaded last Pos.");
 
             PosSaver loader = JsonUtility.FromJson<PosSaver>(loadString);
-            
+
             // Debug.Log($"loaded: {loader.pos} .");
 
-            lastUsedDoor.position = loader.pos;
-            lastUsedDoor.eulerAngles = loader.rot;
+            if (loader != null)
+            {
+
+                lastUsedDoor.position = loader.pos;
+                lastUsedDoor.eulerAngles = loader.rot;
+            }
         }
 
     }
 
     // saves the position and rotation
-    class PosSaver{
+    class PosSaver
+    {
         public Vector3 pos;
         public Vector3 rot;
     }
 
-    void CheckFileDir(){
-        if(!Directory.Exists(fullPath)){
+    void CheckFileDir()
+    {
+        if (!Directory.Exists(fullPath))
+        {
             Directory.CreateDirectory(fullPath);
         }
     }
--- Assets/Scripts/SceneManagementSystem/ChangeScene.cs
+++ Assets/Scripts/SceneManagementSystem/ChangeScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Interactable))]
 public class ChangeScene : MonoBehaviour
 {
 
@@ -12,32 +13,51 @@ public class ChangeScene : MonoBehaviour
 
     [SerializeField] InventoryItem RequiredItem;
     [SerializeField] InventoryManager MainInventory;
+    [SerializeField] GameObject lockedPopUp;
 
-    public void GotToNextRoom(){
+    public void GotToNextRoom()
+    {
 
-        if (RequiredItem && MainInventory){
+        if (RequiredItem && MainInventory)
+        {
             List<string> itemsNames = MainInventory.GetItemNames();
             // the required item is specified
-            if (itemsNames.Contains(RequiredItem.ItemName)){
+            if (itemsNames.Contains(RequiredItem.ItemName))
+            {
                 // the player has the required item
                 GoToRoom();
-            } else {
-                // the player does not have the required item
-                Debug.LogError("Dont have Required Item : " + RequiredItem.ItemName + ".");
             }
-        } else {
+            else
+            {
+                // the player does not have the required item
+                InstantiatePopUp();
+                // Debug.LogError("Dont have Required Item : " + RequiredItem.ItemName + ".");
+            }
+        }
+        else
+        {
             // no required item specified
             GoToRoom();
         }
     }
 
     // Go to the specified room 
-    void GoToRoom(){
-        if (UseIndex){
+    void GoToRoom()
+    {
+        if (UseIndex)
+        {
             SceneManager.LoadScene(NextRoomIndex);
-        } else {
+        }
+        else
+        {
             SceneManager.LoadScene(NextRoomName);
         }
+    }
+
+    void InstantiatePopUp()
+    {
+        GameObject go = Instantiate(lockedPopUp);
+        Destroy(go, 3f);
     }
 
 
